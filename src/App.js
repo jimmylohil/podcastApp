12fa54f048d997f2a5f8e3d47d3bb1b04f555c08
JIMMY <jlohil1505@gmail.com>
2019-07-10 09:14:28 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HeaderComp from './Components/HeaderComp';
+// import NewRealeseComp from './Components/NewRealeseComp';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderComp />
+      {/* <NewRealeseComp /> */}
+    </div>
+  );
+}
+
+export default App;
