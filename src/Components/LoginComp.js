169420705f3c1df6f8bb0ab566fe7d2028c4a778
GoldwinC25 <winzgo25@gmail.com>
2019-08-02 22:54:00 +0700

    Update LoginComp.js

--- src/Components/LoginComp.js
+++ src/Components/LoginComp.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import {component} from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import {Link} from 'react-router-dom';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import {Link as RouterLink, BrowserRouter, Route} from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import bgImg from '../images/bg.jpg';
 import PropTypes from 'prop-types';
-
+import {connect} from 'react-redux';
+import classnames from 'classnames';
+import CategoryWelcomePage from './CategoryWelcomePage';
 import axios from 'axios';
 
+
+
+// For Styling
 const useStyles = theme =>({
   '@global':{
     body : {
@@ -22,18 +32,39 @@ const useStyles = theme =>({
     alignItems : 'center',
     backgroundColor : theme.palette.common.white,
     padding : theme.spacing(5),
-  },
-  form : {
-    width : '100%',
-    marginTop : theme.spacing(1),
-  },
-  submit : {
-    margin : theme.spacing(2,0,2),
-    background: 'linear-gradient(326deg, #050115 0%, #100a26 47%, #3c0b65 95%)',
-  },
-  link : {
-    textDecoration : 'none',
-  },
+  
+},
+form : {
+  width : '100%',
+  marginTop : theme.spacing(1),
+},
+
+submit : {
+  margin : theme.spacing(2,0,2),
+  background: 'linear-gradient(326deg, #050115 0%, #100a26 47%, #3c0b65 95%)',
+
+},
+
+// textfield : {
+//   color : theme.palette.common.white,
+
+//   '& .MuiOutlinedInput-root': {
+//     '& fieldset': {
+//       borderColor: 'grey',
+//     },
+//     '&:hover fieldset': {
+//       borderColor: 'black',
+//     },
+//     '&.Mui-focused fieldset': {
+//       borderColor: 'purple',
+//     },
+//   },
+// }
+
+link : {
+  textDecoration : 'none',
+},
+
 });
 
 const formValid = ({ formErrors, ...rest}) =>{
@@ -48,6 +79,8 @@ const formValid = ({ formErrors, ...rest}) =>{
   });
   return valid;
 }
+
+
 
 class Login extends Component {
   constructor(props){
@@ -64,7 +97,9 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   };
   
+
   onChange = e => {
+    // this.setState({[e.target.id] : e.target.value})
     e.preventDefault();
     const {id, value} = e.target;
     let formErrors = this.state.formErrors;
@@ -87,6 +122,7 @@ class Login extends Component {
     }
 
     this.setState({formErrors, [id] : value}, console.log(this.state));
+
   };
 
   onSubmit = e => {
@@ -99,6 +135,7 @@ class Login extends Component {
 
     else{
       console.error("Form Error");
+
     }
     
     const userData = {
@@ -116,30 +153,47 @@ class Login extends Component {
     .then(
       function(response){
         console.log(response);
+        
         if(response.status == 200 && response.data.error == null){
           console.log("Login successful");
           alert("Login Success");
           let authToken = response.data.token;
           sessionStorage.setItem("JWT", authToken);
           self.props.history.push({
-            pathname:'/categorypage',
+            pathname:'/categorywelcomepage',
             state:{
             username : self.state.username,
           }});
+         
+          // var categoryPage=[];
+          // categoryPage.push(<CategoryWelcomePage appContext={self.props.appContext}/>)
+          //   self.props.appContext.setState({Login:[], categoryPage:categoryPage})
         }
         else if(response.status == 200 && response.data.error == true){
           console.log("Username password do not match");
           alert("Username Password do not match")
         }
+
+      
+        
       }
+      
     )
+
     .catch(function(error)
     {
       console.log(error);
     });
+    
+    
+    
+ 
   };
  
+
+
   render(){
+
     const {classes} = this.props;
     const {formErrors} = this.state;
         return (
@@ -147,6 +201,7 @@ class Login extends Component {
           alignItems="center" justify="center" style={{minHeight: '100vh'}}>
             <div className={classes.paper}>
               <Typography component="h1" variant="h5">Login</Typography>
+
               <form className={classes.form} noValidate onSubmit={this.onSubmit}>
                 <TextField 
                   variant="outlined"
@@ -160,7 +215,9 @@ class Login extends Component {
                   helperText = {formErrors.username}
                   error ={formErrors.username.length === 0 ? false : true}
                   />
-                <TextField 
+                 
+
+                  <TextField 
                   variant="outlined"
                   type="password"
                   label="Password"
@@ -171,28 +228,36 @@ class Login extends Component {
                   onChange = {this.onChange}
                   helperText = {formErrors.password}
                   error ={formErrors.password.length === 0 ? false : true}
-                />    
-                <Link to="/categorypage" className={classes.link}>
+                  />
+                  
+
+
                   <Button className={classes.submit}
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    >Login</Button>
-                </Link>         
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  >Login</Button>
               </form>
 
               <Grid container>
+                
+
                 <Grid item>
-                  <Link to="/register" className={classes.link}>
-                      {"Don't have an account ? Sign Up"}
-                  </Link>
+                    <RouterLink to="/register" className={classes.link}>
+                    <Link variant="body2" >
+                          {"Don't have an account ? Sign Up"}
+                      </Link>
+                    </RouterLink>
                 </Grid>
+
               </Grid>
             </div>
+
           </Grid>
         );
   }
+      
 }
 
 Login.propTypes ={
