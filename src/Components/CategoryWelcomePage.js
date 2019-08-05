4a32b780ca8fdeb6ebf2c47b136a27403fca3da9
GoldwinC25 <winzgo25@gmail.com>
2019-08-05 23:27:41 +0700

    Update CategoryWelcomePage.js

--- src/Components/CategoryWelcomePage.js
+++ src/Components/CategoryWelcomePage.js
@@ -2,25 +2,32 @@ import React from 'react';
 import {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid';
-import {  withStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
+import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 import Checkbox from '@material-ui/core/Checkbox';
 import Chip from '@material-ui/core/Chip';
+import img from '../images/bg.jpg';
+import education from '../images/education.jpg';
+import music from '../images/music.jpg';
 import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
+import Login from './LoginComp';
+import {withRouter} from 'react-router-dom';
 import axios from 'axios';
-import { isTSEnumMember } from '@babel/types';
-
-import {Link } from 'react-router-dom';
-
-import auth from './auth';
-
-
 
 
 const useStyles = theme =>({
@@ -41,7 +48,7 @@ root :{
 searchbox: {
     marginTop : theme.spacing(3),
     backgroundColor : '#764ba2',
-    width : '100%',
+    width : '80%',
     
 },
 input : {
@@ -69,8 +76,9 @@ media : {
     height : '100px',
 },
 
-chiplist : {
+paperlist : {
     marginTop : '20px',
+    width : '93%',
     paddingTop : '10px',
     paddingBottom : '10px',
     paddingLeft : '5px',
@@ -81,55 +89,68 @@ chiplist : {
 });
 
 
-
 class CategoryWelcomePage extends Component{
-    
-    constructor(props) {
-        super(props)
+    constructor(props){
+        super(props);
         this.state = {
-             category: []
+            category : [],
+            preferred : []
+        };
+        this.onChange = this.onChange.bind(this);
+    };
+
+    onChange = e =>{
+        const preferred = this.state.preferred
+        let index
+
+        //check if the check box is checked or unchecked
+        if(e.target.checked){
+            preferred.push(+e.target.value)
         }
-    }
-    
+        else{
+            index = preferred.indexOf(+e.target.value)
+            preferred.splice(index,1)
+        }
+
+        this.setState({preferred: preferred})
+        
+        console.log (this.state.preferred);
+    };
+
+
 
     componentDidMount(){
-        
-        axios.get("http://localhost:8000/api/category/list?token=".concat(sessionStorage.getItem("JWT")))
+        axios.get('http://localhost:80/api/category/list?token='.concat(sessionStorage.getItem("JWT")))
         .then(
-        (response) => {
-                console.log("Get Category successful")
+            (response) => {
+                console.log("Get category successful")
                 console.log(response)
-                this.setState (
+                this.setState(
                     {category:response.data.categories}
                 )
-                console.log (auth.isLogin());
-                console.log("category : "+ this.state.category)
-
-        }
+                // console.log(this.state.category)
+            }
         )
-        .catch(function(error)
-        {
-        console.log(error);
-        });
-        
-    }
 
+        .catch(function(error){
+            console.log(error);
+        })
+        
+    };
 
     render(){
-        const {classes} = this.props;
         const {category} = this.state;
+        const {classes} = this.props;
+        const {preferred} = this.state;
             return (
                 <Container component="main" spacing={0} justify="center" style={{minHeight: '100vh', width : '80%'}}> 
             
                     <div className={classes.root}>
                         <Typography component="h1" variant="h2">
-                            What are you interested in ? 
+                            What are you interested in ?
                         </Typography>
                         <Typography component="h2" variant="h3">
-                            
-
                             Haiii ! {this.props.location.state.username}
-                            
                         </Typography>
         
                         <Paper className={classes.searchbox}>
@@ -142,29 +163,158 @@ class CategoryWelcomePage extends Component{
                             </IconButton>
                         </Paper>
         
-                        <Chip
-                        label="Deletable Chip"
-                        onDelete="{handleDelete}"
-                        className={classes.chiplist}
-                        />
-                        {
-                            category.map (item =>
-                            <h1>{item}</h1>
-                            )
-                            
-                        }
+                        <Paper className={classes.paperlist}>
+                            {preferred.map(item =>{
+                                return(
+                                    <Chip
+                                    label={item}
+                                    />
+                                )
+                            })}
+                            <Chip
+                            label="Deletable Chip"
+                            onDelete="{handleDelete}"
+                            className="{classes.chip}"
+                            />
+                        </Paper>
+        
+                        <Grid container maxWidth="lg"> 
 
-                        <Link to="/home">
+                            {
+                                category.map((item, i) =>
+                                    {   
+                                        console.log(item)
+                                        return(
+                                            <Card className={classes.card}>
+                                                <CardMedia
+                                                className={classes.media}
+                                                image={education}
+                                                title={item}
+                                                key={i}
+                                                />
+
+                                                <CardContent className={classes.content}>
+                                                    <Typography component="h2" variant="h5">{item}
+                                                        <Checkbox
+                                                        key={i}
+                                                        name={item}
+                                                        value={item}
+                                                        label={item}
+                                                        onClick={this.onChange} />
+                                                    </Typography>
+                                                </CardContent>
+                                                
+                                            </Card>
+                                            
+                                        );
+                                        
+                                    })
+                            }
+                        
+                            {/* <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={education}
+                                title="Education"/>
+
+                                <CardContent className={classes.content}>
+                                    <Typography component="h2" variant="h5">Education
+                                        <Checkbox 
+                                        value="checked" />
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+        
+        
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={music}
+                                title="Music"
+                                />
+
+                                <CardContent>
+                                    <Typography component="h2" variant="h5">Music
+                                        <Checkbox 
+                                        value="checked" />
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={music}
+                                title="Music"
+                                />
+
+                                <CardContent>
+                                    <Typography component="h2" variant="h5">Music
+                                        <Checkbox 
+                                        value="checked" />
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            
+                
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={music}
+                                title="Music"
+                                />
+
+                                <CardContent>
+                                    <Typography component="h2" variant="h5">Technology
+                                        <Checkbox 
+                                        value="checked" />
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            
+        
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={music}
+                                title="Music"
+                                />
+
+                                <CardContent>
+                                    <Typography component="h2" variant="h5">Humour
+                                        <Checkbox 
+                                        value="checked" />
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={music}
+                                title="Music"
+                                />
+
+                                <CardContent>
+                                    <Typography component="h2" variant="h5">Business
+                                        <Checkbox 
+                                        value="checked" />
+                                    </Typography>
+                                </CardContent>
+                            </Card> */}
+                          
+                        </Grid>
+        
                         <Button className={classes.submit}
                           type="submit"
                           variant="contained"
                           color="primary">Continue</Button>
-                        </Link>
 
                         <Button className={classes.submit}
                           onClick={this.onLogoutClick}
                           variant="contained"
                           color="primary">Logout</Button>
+        
                     </div>
     
                   </Container>
@@ -181,4 +331,4 @@ CategoryWelcomePage.propTypes = {
 
 
 
-export default withStyles(useStyles)(CategoryWelcomePage);
+export default withRouter(withStyles(useStyles)(CategoryWelcomePage));
